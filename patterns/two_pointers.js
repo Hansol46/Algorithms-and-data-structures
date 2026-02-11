9f8a95d8015fc759756ccc9036361dab20ec0b55
Yury Kiryaev <kiryaev.yury@yandex.ru>
2026-02-11 11:04:04 +0300

    Implement two pointers algorithm examples
    Added two pointers algorithm examples for palindrome checking and two-sum problem.

--- patterns/two_pointers.js
+++ patterns/two_pointers.js
@@ -1,3 +1,65 @@
 /**
-* Two pointers - 
+* Two pointers - указатели двигаеются на встречу друг другу или по условию.
+* Когда думаем про two pointers ?
+* - Массив отсортирован
+* - Нужно найти пару
+* - Нужно сравнить начало и конец
+* - Нужно сжать пространство поиска
+
+* Template: 
+* let left = 0;
+* let right = arr.length - 1;
+*
+* while (left < right) {
+*   if (условие выполнено) {
+*     // делаем что нужно
+*   }
+* 
+*   if (что-то меньше) {
+*     left++;
+*   } else {
+*     right--;
+*   }
+* }
 */ 
+
+/**
+* Алгоритмическая сложность: O(n)
+* Пространственная сложность: O(1)
+*/
+function isPalindrome(string){
+  let left = 0;
+  let right = string.length - 1;
+
+  while(left < right) {
+    if(string[left] !== string[right]){
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true
+}
+
+const arr = [1,2,4,6,10]
+/**
+* Алгоритмическая сложность: O(n)
+* Пространственная сложность: O(1)
+*/
+function twoSum(arr, target = 8){
+  let left = 0;
+  let right = arr.length - 1;
+  while(left < right) {
+    const sum = arr[left] + arr[right];
+    
+    if(sum === target) {
+      return [left, right]
+    }
+
+    if(sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+}
