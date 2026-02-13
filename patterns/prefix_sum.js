2cf4182bb7af49176adf7920d7c48c63e7694e91
Yury Kiryaev <kiryaev.yury@yandex.ru>
2026-02-13 16:32:06 +0300

    Create prefix_sum.js

---
+++ patterns/prefix_sum.js
@@ -0,0 +1,41 @@
+/**
+* Prefix sum - особый подход, чтобы можно было посчитать накопленные суммы.
+* Пример:
+* nums:   [3, 2, 4, 1, 5]
+* prefix: [3, 5, 9, 10, 15]
+*   getSum(1,3) = prefix[3] - prefix[1 - 1] = 7
+
+* Также prefix sum нужен для поиска подмассивов с заданной суммой
+* Пример дан массив [1, 2, 3], нужно посчитать сколько подмассивов имеют сумму k, k = 3.
+* Ответ: [1,2], [3] - 2 подмассива
+
+* Template: 
+* 1️⃣ currentSum += nums[i]
+* 2️⃣ если map содержит (currentSum - k)
+*      count += map[currentSum - k]
+* 3️⃣ map[currentSum]++
+*/ 
+
+/**
+* Subarray Divisible by K
+* Алгоритмическая сложность: O(n)
+* Пространственная сложность: O(1)
+*/
+const arr = [4,5,0,-2,-3,1]
+function subarrayDevideByK(arr, k = 5) {
+  let count = 0;
+  let map = {0: 1}
+  let currentSum = 0;
+  for(let i = 0; i < arr.length; i++){
+    currentSum += arr[i];
+    let key = currentSum % k
+    if(key < 0) {
+      key += k
+    }
+    if(map[key]) {
+      count += map[key];
+    }
+    map[key] = (map[key] || 0) + 1
+  }
+  return count;
+}
