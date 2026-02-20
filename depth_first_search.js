95c71ec727cb00f1aa17f641f25c08373c2f022a
Yury Kiryaev <kiryaev.yury@yandex.ru>
2026-02-20 17:19:53 +0300

    Implement island counting using depth-first search
    Added a function to count islands in a grid using DFS.

--- depth_first_search.js
+++ depth_first_search.js
@@ -6,6 +6,41 @@
 */
 
 /**
+* Найти и вернуть кол-во островов
+*/
+const grid = [ 
+  ['1','1','0'], 
+  ['0','1','0'], 
+  ['1','0','1'] 
+];
+function numsIsLand(grid) {
+  let count = 0;
+    
+  function dfs(r,c){
+      if(r < 0 || c < 0 || r >= grid.length || c >= grid[0].length || grid[r][c] === '0') {
+          return;
+      }
+      
+      grid[r][c] = '0';
+
+      dfs(r + 1,c);
+      dfs(r - 1,c);
+      dfs(r,c + 1);
+      dfs(r,c - 1);
+  }
+
+  for(let r = 0; r < grid.length; r++) {
+      for(let c = 0; c < grid[r].length; c++) {
+          if(grid[r][c] === '1') {
+              count++;
+              dfs(r,c);
+          }
+      }
+  }
+  return count;  
+}
+
+/**
  *          A
  *         / \
  *        B   C
