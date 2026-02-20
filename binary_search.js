675701cd1e33939cd1923cae6f33125cc1e6d6e2
Yury Kiryaev <kiryaev.yury@yandex.ru>
2026-02-20 09:27:21 +0300

    Implement binary search for first and last positions
    Added functions to find the first and last positions of a target in a sorted array using binary search.

--- binary_search.js
+++ binary_search.js
@@ -1,9 +1,11 @@
 const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]; 
 
 /**
+ * Важно: только в отсортированном массиве
  * Сложность алгоритма: O(logn), 
  * где n - количество элементов в массиве.
  * Для массива из 10 элементов понадобится примерно 4 итерации, для массива из 1000 элементов примерно 10.
+ * Вернуть index 
  */
 function binarySearch(array, target) {
   let left = 0;
@@ -12,7 +14,7 @@ function binarySearch(array, target) {
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
     if (array[mid] === target) {
-      return array[mid];
+      return mid;
     }
     if (array[mid] < target) {
       left = mid + 1; 
@@ -21,4 +23,70 @@ function binarySearch(array, target) {
     }
   }
   return -1;
+}
+
+/**
+* Задача: Найти первую позицию target
+* nums = [1,2,2,2,3] target = 2
+* Ответ: 1
+*/
+const nums = [1,2,2,2,3];
+function binarySearchFindFirstPos(nums, target = 2) {
+  let left = 0;
+  let right = nums.length - 1;
+  let index = -1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    
+    if (nums[mid] === target) {
+      index = mid;
+      right = mid - 1;
+    } else if (nums[mid] < target) {
+      left = mid + 1; 
+    } else {
+      right = mid - 1;
+    }
+  }
+  
+  return index;
+};
+
+/**
+* Задача: Найти последнюю позицию target
+* nums = [1,2,2,2,3] target = 2
+* Ответ: 3
+*/
+const nums = [1,2,2,2,3];
+function binarySearchFindLastPos(nums, target = 2) {
+  let left = 0;
+  let right = nums.length - 1;
+  let index = -1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    
+    if (nums[mid] === target) {
+      index = mid;
+      left = mid + 1;
+    } else if (nums[mid] < target) {
+      left = mid + 1; 
+    } else {
+      right = mid - 1;
+    }
+  }
+  
+  return index;
+};
+
+/**
+* Ищем и возвращаем [первая позиция, последняя позиция]
+* Алгоритмическая сложность: O(logn) + O(logn) = O(logn)
+* Пространственная сложность: O(1)
+* Вводные данные: nums = [1,2,2,2,3], target = 2
+* Ответ: [1,3]
+*/ 
+const nums = [1,2,2,2,3];
+function searchRange(nums, target = 2){
+  return [binarySearchFindFirstPos(nums,target),binarySearchFindLastPos(nums,target)]
 }
