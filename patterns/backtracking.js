4773ffb44acdb2f689dc06dbb7bc322ce7c1c772
Yury Kiryaev <kiryaev.yury@yandex.ru>
2026-02-28 10:55:01 +0300

    Implement permutation function with backtracking
    Added a permutation function using backtracking.

--- patterns/backtracking.js
+++ patterns/backtracking.js
@@ -48,3 +48,34 @@ function subsets(nums) {
   backtrack(0,[]);
   return result
 }
+
+/**
+* Permutation - частный лучай backtracking
+* Алгоритмическая сложность: O(n!)
+*/ 
+function permute(nums) {
+  const result = [];
+  const used = new Array(nums.length).fill(false);
+
+  function backtrack(path) {
+    if (path.length === nums.length) {
+      result.push([...path]);
+      return;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      if (used[i]) continue;
+
+      used[i] = true;
+      path.push(nums[i]);
+
+      backtrack(path);
+
+      path.pop();
+      used[i] = false;
+    }
+  }
+
+  backtrack([]);
+  return result;
+}
