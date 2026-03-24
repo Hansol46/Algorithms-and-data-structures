918343723aefe0848b992f04c1b7d213b8de2b95
Yury Kiryaev <kiryaev.yury@yandex.ru>
2026-03-24 12:16:02 +0300

    Create merge_sort.js

---
+++ sorting/merge_sort.js
@@ -0,0 +1,32 @@
+/**
+* Merge sort - склеиваем два списка, важно чтобы они были отсортированы.
+* Сложность O(n + m) -> O(n)
+* Память O(n)
+*/
+function mergeSort(arr1, arr2) {
+  const result = []
+  let i = 0;
+  let j = 0;
+  
+  while(i < arr1.length && j < arr2.length) {
+    if(arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+
+  while(i < arr1.length) {
+     result.push(arr1[i]);
+     i++
+  }
+  
+  while(j < arr2.length) {
+     result.push(arr2[j]);
+     j++;
+  }
+  
+  return result;
+}
