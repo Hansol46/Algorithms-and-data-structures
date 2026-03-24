7cb406fed9cc3b215cd6026e26fe19ef9f222f91
Yury Kiryaev <kiryaev.yury@yandex.ru>
2026-03-24 11:26:40 +0300

    Update linear_search.js

--- searching/linear_search.js
+++ searching/linear_search.js
@@ -14,26 +14,3 @@ const linearSearch = (array, item) => {
 
   return -1;
 };
-
-/**
-* Простейшая функция RLE(Run-Length Encoding)
-*/
-function rle(string) {
-  const result = [];
-  let lastChar = string[0]
-  let lastPos = 1; 
-
-  for(let i = 1; i < string.length; i++) {
-     if(string[i] != lastChar) {
-       result.push(lastChar + lastPos);
-       lastChar = string[i];
-       lastPos = 1;
-     } else { 
-      lastPos ++;
-     }
-  }
-
-  result.push(lastChar + lastPos)
-  return result.join("") 
-}
-console.log(rle('AAABBCDDEEEFFAAAAA')) // "A3B2C1D2E3F2A5"
